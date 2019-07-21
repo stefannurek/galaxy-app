99cf79696fdf166310c5ecf0d7e8909ce0e21291
stefan <sawicadam@gmail.com>
2019-07-21 15:14:58 +0200

    added PlanetController

---
+++ src/main/java/galaxyapp/controllers/PlanetController.java
@@ -0,0 +1,27 @@
+package galaxyapp.controllers;
+
+
+import galaxyapp.models.Planet;
+import galaxyapp.services.PlanetService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class PlanetController {
+
+    PlanetService planetService;
+
+    public PlanetController( PlanetService planetService){
+        this.planetService=planetService;
+    }
+
+
+    @GetMapping("/planet")
+    public ResponseEntity<Planet> getPlanetByName(@RequestParam(value = "name") String planetName){
+        return new ResponseEntity<> (planetService.getPlanetByName(planetName), HttpStatus.OK);
+    }
+
+}
